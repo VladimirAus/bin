9f6379e4ad78ced0089e62b4f4b27e201007abd2
VladimirAus <v_roudakov@yahoo.com>
2014-10-31 08:18:26 +1000

    Composer and Drush scripts update

--- setup/install_brew_cmpr_drush.sh
+++ setup/install_brew_cmpr_drush.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 composer global require drush/drush:dev-master
+sudo ln -s ~/.composer/vendor/bin/drush /usr/bin/drush7
--- setup/install_brew_composer.sh
+++ setup/install_brew_composer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 # Install composer
-brew install composer --ignore-dependencies
+# brew install composer --ignore-dependencies
+# curl -sS https://getcomposer.org/installer | php
+brew update
+brew tap homebrew/dupes
+brew tap homebrew/php
+brew install php55
+brew install composer
 composer global update
